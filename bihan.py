6939a6ed7c16db6b0f0868b232e03252e5284b52
Pierre Quentel <pierre.quentel@orange.com>
2017-03-26 22:35:30 +0200

    Don't expose functions defined in a module with __expose__ set to False

--- bihan.py
+++ bihan.py
@@ -331,9 +331,17 @@ class application(http.server.SimpleHTTPRequestHandler):
                 prefix = "/" + module.__prefix__.lstrip("/")
             for key in dir(module):
                 obj = getattr(module, key)
+                # Functions exposed are those whose name doesn't start with _,
+                # don't have an attribute __expose__ set to False, and are not
+                # defined in a module that has a name __expose__ set to False.
                 if (type(obj) is types.FunctionType 
                         and not key.startswith("_") 
-                        and getattr(obj, '__expose__', True)):
+                        and getattr(obj, '__expose__', True)
+                        and (obj.__module__ in sys.modules
+                             and getattr(sys.modules[obj.__module__], 
+                                 '__expose__', True)
+                            )
+                    ):
                     url = obj.url if hasattr(obj, "url") else "/" + key
                     url = "/" + (prefix + url).lstrip("/")
                     pattern = re.sub('<(.*?)>', r'(?P<\1>[^/]+?)', url)
